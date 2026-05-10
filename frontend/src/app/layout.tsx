bf69d7acc567076d888aa38c337e79da57b1a2a5
JALAL <jalaluddin0046356@gmail.com>
2026-05-10 18:58:07 +0600

    Fixed the error issues

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -13,8 +13,8 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode; }) { 
   return (
-    <html lang="en">
-      <body className={`${inter.className} bg-light-violet`}>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} bg-light-violet`} suppressHydrationWarning>
         <AuthProvider>
           {children}
         </AuthProvider>
--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -9,11 +9,13 @@ const api = axios.create({
 api.interceptors.request.use(
   (config) => {
 
-    const token = localStorage.getItem('token');
+    if (typeof window !== 'undefined') {
+      const token = localStorage.getItem('token');
     
-    if (token) {
+      if (token) {
       
-      config.headers.Authorization = `Bearer ${token}`;
+        config.headers.Authorization = `Bearer ${token}`;
+      }
     }
     
     return config;
